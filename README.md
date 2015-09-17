e07f742f5248cc3812cdce92b868999d249db4ec
yieme <yieme@cormea.com>
2015-09-17 15:48:01 -0600

    initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# imageup
+# imageup <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="165">
 Bounce Up Image Sizes Based on Screen Width
+
+## Install
+
+```js
+npm install imageup --save
+```
+
+## License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// imageup - Bounce Up Image Sizes Based on Screen Width - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var Imageup = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = Imageup // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return Imageup }) // support AMD
+  // else this.Imageup = Imageup // support browser
+})();
