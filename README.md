76395c2730927f3cae3a3225421be72ad636e3a5
yieme <yieme@cormea.com>
2015-09-17 18:28:12 -0600

    initial-debugs

--- README.md
+++ README.md
@@ -1,10 +1,52 @@
 # imageup <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="165">
 Bounce Up Image Sizes Based on Screen Width
 
-## Install
+## Use
+### Example 1
+Mobile first image which doubles as mirage style image until hi-res image is loaded. `noscript` safe and no changes are made if the screen width is less than the first breakpoint.
 
-```js
-npm install imageup --save
+```html
+<img src='img/320/sample.jpg'>
+<script src="imageup.js"></script>
+<script>
+    ImageUp({
+        init: 1,        // truthy means to set these values as the default configuration
+        tag:  'img',    // img is the default if tag is undefined
+        attr: 'src',    // src is the default if attr is undefined
+        ref:  '/320/',  // {src} is the default if ref undefined
+        640:  '/640/',  // break point and associated value to replace the ref '{src}' in the tag.attr (ex: img.src)
+        1280: '/1280/'  // example medium screen
+    }
+    ImageUp({ tag: 'body',    attr: 'defaultBackgroundImg' }); // apply to special page background image using the other
+    ImageUp({ tag: 'article', attr: 'customBackgroundImg'  }); // apply to special article background images
+</script>
 ```
+
+#### Example 2
+Don't double loading images. Note every breekpoint is needed
+
+```html
+<img data-src='img/{src}/sample.jpg'>
+<script src="imageup.js"></script>
+<script>
+  ImageUp({ 1: 'small', 640: 'medium', 1280, 'large' }); // use words instead of numbers in the path
+</script>
+```
+
+#### Example 3
+Detect what image size was applied
+
+```html
+<script>
+  var size = ImageUp({ 1: 'small', 640: 'medium', 1280, 'large' });
+  console.log(size) // undefined if no match, 1, 640, 1280 depending upon the device screen width
+</script>
+```
+
+## How it works
+- If the screen width falls within a break point:
+  - For each page element that is of the `tag` type:
+    - If there is a matching `attr` tag or `data-` `attr` tag:
+      - Replace the `ref` value in the element attribute with the associated break point string
 
 ## License: MIT
--- imageup.js
+++ imageup.js
@@ -1,61 +1,59 @@
-// ImageUp
-(function(doc, win, Module, GetElementsByTagName, IsInteger, lastResize, init, min, max, key, ref, size, sizes, width, Exports, elements, element, attribute, tagName, imageUpAttribute, imageUpAttributeSize, imageUpAttributeOrig) {
-	GetElementsByTagName = doc.getElementsByTagName;
-	IsInteger            = Number.isInteger;
-	init                 = {
-		tag:  'img',
-		attr: 'src',
-		ref:  '{src}'
-	};
-	imageUpAttribute      = 'ImageUp-';
-	imageUpAttributeSize = imageUpAttribute+'siz';
-	imageUpAttributeOrig = imageUpAttribute+'src';
+// ImageUp 1.0.2
+(function(lastResize, init, min, max, key, ref, size, sizes, width, Exports, elements, element, attribute, tagName, imageUpAttribute, imageUpAttributeSize, imageUpAttributeOrig) {
+	init                 = {};
+	imageUpAttribute     = 'ImageUp';
+	imageUpAttributeSize = imageUpAttribute+'Siz';
+	imageUpAttributeOrig = imageUpAttribute+'Src';
 
 	// upgrade images
 	function ImageUp(param) {
 		param = param || {};
 		if (param.init) {
-			init = param.init;
+			init = param;
 			return;
 		}
 		sizes     = [];
-		tagName   = param.tag  || init.tag;
-		attribute = param.attr || init.attr;
-		ref       = param.ref  || init.ref;
+		tagName   = param.tag  || init.tag  || 'img';
+		attribute = param.attr || init.attr || 'src';
+		ref       = param.ref  || init.ref  || '{src}';
 		max       = 0;
 		min       = 1e9;
 		width     = screen.width;
 
-		// get ordered size boundaries
-		for(key in param) {
-			if (IsInteger(key)) {
+		// Find size boundaries
+		function FindMinMax(obj) {
+			for(key in obj) {
 				key = parseInt(key);
-				if (key > 0) {
-					if (key < min) min = key;
-					if (key <= width && key >= max) max = key;
+				if (key) {
+					if (key > 0) {
+						if (key < min) min = key;
+						if (key <= width && key >= max) max = key;
+					}
 				}
 			}
 		}
+		FindMinMax(init);
+		FindMinMax(param);
 
 		if (width < min) return; // no changes
 
-		elements  = GetElementsByTagName(tagName);
+		elements  = document.getElementsByTagName(tagName);
 		for (var i = 0; i < elements.length; i++) {
 			element = elements[i];
 			size    = element[imageUpAttributeSize] || 0;
 			if (max > size) {
 				element[imageUpAttributeSize] = max; // save the size for future checks, ie reorient
-				key = element[imageUpAttributeOrig] || element[attribute] || element['data-' + attribute];
-				element[imageUpAttributeOrig] = key; // save for future checks
-				element[attribute] = key.replace(ref, param[max]);
+				key = element[imageUpAttributeOrig] || element[attribute] || element.dataset[attribute];
+				if (key) {
+					element[imageUpAttributeOrig] = key; // save for future checks
+					key = key.replace(ref, param[max] || init[max]);
+					element[attribute] = key;
+					element.dataset[attribute] = key;
+				}
 			}
 		}
+		return max;
 	}
 
-	// exportable module
-	if (Module && Module.exports) {
-		Module.exports = ImageUp;
-	} else { // make global and call
-		win.ImageUp = ImageUp;
-	}
-})(document, window, module);
+	window[imageUpAttribute] = ImageUp;
+})();
